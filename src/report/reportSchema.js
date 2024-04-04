bc747faf33dfd62bfa1feec973e7c809cb707cce
parkjiwonnn <jiwonp05@gmail.com>
2024-04-04 15:50:25 +0900

    ✨feat : reportSchema 완성

---
+++ src/report/reportSchema.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+const reportSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: mongoose.Types.ObjectId,
+      ref: 'image',
+      required: true,
+    },
+    all_veg: {
+      type: Boolean,
+      required: true,
+    },
+    tel: {
+      type: String,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    address_detail: {
+      type: String,
+    },
+    location: {
+      type: {
+        type: String, // GeoJSON 타입
+        enum: ['Point'], // 'location.type'은 'Point'만 가능
+        required: true,
+      },
+      coordinates: {
+        type: [Number], // [경도, 위도]
+        required: true,
+      },
+    },
+    hours: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
+    user_email: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+reportSchema.index({ location: '2dsphere' });
+
+const Report = mongoose.model('Report', reportSchema);
+
+module.exports = Report;
