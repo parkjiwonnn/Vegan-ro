e12012bd057c4a33416a6d1ff42c73b274022d1d
[SW8] 이진주 <dsa0362@gmail.com>
2024-04-11 19:12:38 +0900

    �fix : vercel 버그 수정

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -20,19 +20,20 @@ const authenticate = (req, res, next) => {
 const isAuthenticated = (req, res, next) => {
   const token = req.headers['authorization'].slice(7);
 
-  if (token == null) {
+  if (!token) {
     return res
       .status(401)
       .json(errors.buildResponse({ message: '토큰이 없습니다.' }));
   }
 
-  jwt.verify(token, JWT_SECRET, (err, user) => {
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) {
       return res
         .status(403)
         .json(errors.buildResponse({ message: '토큰 인증에 실패했습니다.' }));
     }
-    req.user = user;
+
+    req.user = decoded;  // 요청 객체에 사용자 정보 추가
     next();
   });
 };
--- src/middleware/validation/bookmark-validation-schema.js
+++ src/middleware/validation/bookmark-validation-schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const bookmarkValidationSchema = Joi.object({
     place_id: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(), // MongoDB ObjectId 형식
-    user_id: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(), // MongoDB ObjectId 형식
+    user_id: Joi.optional(),
 });
 
 module.exports = bookmarkValidationSchema;
