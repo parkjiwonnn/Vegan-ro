2f2c759f2f6caad8690b4ba181cbe3d7e588f9aa
parkjiwonnn <jiwonp05@gmail.com>
2024-04-17 11:56:57 +0900

    ✨feat : report joi 유효성 검사 형식 수정

--- src/middleware/validation/place-validation-schema.js
+++ src/middleware/validation/place-validation-schema.js
@@ -5,7 +5,12 @@ const placeValidationSchema = Joi.object({
   category: Joi.string().required(),
   // category_img: req.body에서 받아온 category로 service에서 category_img 이미지 컬렉션에서 찾아서 저장
   vegan_option: Joi.boolean().required(),
-  tel: Joi.string().allow('').default(''),
+  tel: Joi.string()
+    .allow('')
+    .pattern(
+      /^(\d{2}-\d{4}-\d{4}|\d{3}-\d{4}-\d{4}|\d{4}-\d{4}-\d{4}|\d{2}-\d{3}-\d{4}|\d{3}-\d{3}-\d{4})$/,
+    )
+    .default(''),
   address: Joi.string().required(),
   address_lot_number: Joi.string().required(),
   address_detail: Joi.string().allow('').default(''),
--- src/middleware/validation/report-validaton-schema.js
+++ src/middleware/validation/report-validaton-schema.js
@@ -5,7 +5,12 @@ const reportedPlaceValidationSchema = Joi.object({
   category: Joi.string().required(),
   // category_img: req.body에서 받아온 category로 service에서 category_img 이미지 컬렉션에서 찾아서 저장
   vegan_option: Joi.boolean().required(),
-  tel: Joi.string().allow('').default(''),
+  tel: Joi.string()
+    .allow('')
+    .pattern(
+      /^(\d{2}-\d{4}-\d{4}|\d{3}-\d{4}-\d{4}|\d{4}-\d{4}-\d{4}|\d{2}-\d{3}-\d{4}|\d{3}-\d{3}-\d{4})$/,
+    )
+    .default(''),
   address: Joi.string().required(),
   address_lot_number: Joi.string().required(),
   address_detail: Joi.string().allow('').default(''),
@@ -17,7 +22,7 @@ const reportedPlaceValidationSchema = Joi.object({
     .length(2)
     .required(),
   open_times: Joi.array().items(Joi.string()).default([]),
-  sns_url: Joi.array().items(Joi.string()).default([]),
+  sns_url: Joi.string().allow(''), // string으로 받아서 배열로 저장
   // user_id: 토큰으로 사용자 ID 저장
 });
 
